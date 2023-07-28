4d331dcfd9d05e7dc0dafd83d67729057f2f39a6
Will <will8086@gmail.com>
2023-07-28 14:40:40 +0800

    樣式完成

--- update.php
+++ update.php
@@ -91,54 +91,55 @@ $conn->close();
                 <span class="input-group-text text-light bg-primary fw-bold rounded-start-4">喜愛的食物種類</span>
                     <input type="checkbox" class="btn-check" id="btn-check-1" name="likefoodtag[]"
                     value="1" <?=in_array(1, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-1">台式</label><br>
+                    <label class="btn btn-outline-warning ms-2 rounded rounded-4 fw-bold" for="btn-check-1">
+                        台式</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-2" name="likefoodtag[]"
                     value="2" <?=in_array(2, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-2">中式</label><br>
+                    <label class="btn btn-outline-warning ms-2 rounded rounded-4 fw-bold" for="btn-check-2">中式</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-3" name="likefoodtag[]"
                     value="3" <?=in_array(3, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-3">日式</label><br>
+                    <label class="btn btn-outline-warning ms-2 rounded rounded-4 fw-bold" for="btn-check-3">日式</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-4" name="likefoodtag[]"
                     value="4" <?=in_array(4, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-4">韓式</label><br>
+                    <label class="btn btn-outline-warning ms-2 rounded rounded-4 fw-bold" for="btn-check-4">韓式</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-5" name="likefoodtag[]"
                     value="5" <?=in_array(5, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-5">港式</label><br>
+                    <label class="btn btn-outline-warning ms-2 rounded rounded-4 fw-bold" for="btn-check-5">港式</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-6" name="likefoodtag[]"
                     value="6" <?=in_array(6, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-6">美式</label><br>
+                    <label class="btn btn-outline-warning ms-2 rounded rounded-4 fw-bold" for="btn-check-6">美式</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-7" name="likefoodtag[]"
                     value="7" <?=in_array(7, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-7">義式</label><br>
+                    <label class="btn btn-outline-warning ms-2 rounded rounded-4 fw-bold" for="btn-check-7">義式</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-8" name="likefoodtag[]"
                     value="8" <?=in_array(8, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-8">法式</label><br>
+                    <label class="btn btn-outline-warning ms-2 rounded rounded-4 fw-bold" for="btn-check-8">法式</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-9" name="likefoodtag[]"
                     value="9" <?=in_array(9, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-9">西式</label><br>
+                    <label class="btn btn-outline-warning ms-2 rounded rounded-4 fw-bold" for="btn-check-9">西式</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-10" name="likefoodtag[]"
                     value="10" <?=in_array(10, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-10">泰式</label><br>
+                    <label class="btn btn-outline-warning ms-2 rounded rounded-4 fw-bold" for="btn-check-10">泰式</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-11" name="likefoodtag[]"
                     value="11" <?=in_array(11, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-11">越式</label><br>
+                    <label class="btn btn-outline-warning ms-2 rounded rounded-4 fw-bold" for="btn-check-11">越式</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-12" name="likefoodtag[]"
                     value="12" <?=in_array(12, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-12">火鍋</label><br>
+                    <label class="btn btn-outline-danger ms-2 rounded rounded-4 fw-bold" for="btn-check-12">火鍋</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-13" name="likefoodtag[]"
                     value="13" <?=in_array(13, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-13">燒烤</label><br>
+                    <label class="btn btn-outline-danger ms-2 rounded rounded-4 fw-bold" for="btn-check-13">燒烤</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-14" name="likefoodtag[]"
                     value="14" <?=in_array(14, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-14">牛排</label><br>
+                    <label class="btn btn-outline-danger ms-2 rounded rounded-4 fw-bold" for="btn-check-14">牛排</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-15" name="likefoodtag[]"
                     value="15" <?=in_array(15, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-15">熱炒</label><br>
+                    <label class="btn btn-outline-danger ms-2 rounded rounded-4 fw-bold" for="btn-check-15">熱炒</label><br>
                 </div>
                 <div class="d-flex mt-2" style="margin-left: 136px;">
                     <input type="checkbox" class="btn-check" id="btn-check-16" name="likefoodtag[]"
                     value="16" <?=in_array(16, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-16">素食</label><br>
+                    <label class="btn btn-outline-success ms-2 rounded rounded-4 fw-bold" for="btn-check-16">素食</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-17" name="likefoodtag[]"
                     value="17" <?=in_array(17, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
                     <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-17">飲品</label><br>
@@ -150,16 +151,16 @@ $conn->close();
                     <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-19">果汁</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-20" name="likefoodtag[]"
                     value="20" <?=in_array(20, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-20">咖啡</label><br>
+                    <label class="btn btn-outline-secondary ms-2 rounded rounded-4 fw-bold" for="btn-check-20">咖啡</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-21" name="likefoodtag[]"
                     value="21" <?=in_array(21, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
                     <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-21">茶</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-22" name="likefoodtag[]"
                     value="22" <?=in_array(22, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-22">炸物</label><br>
+                    <label class="btn btn-outline-danger ms-2 rounded rounded-4 fw-bold" for="btn-check-22">炸物</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-23" name="likefoodtag[]"
                     value="23" <?=in_array(23, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-23">吃到飽</label><br>
+                    <label class="btn btn-outline-dark ms-2 rounded rounded-4 fw-bold" for="btn-check-23">吃到飽</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-24" name="likefoodtag[]"
                     value="24" <?=in_array(24, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
                     <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-24">小吃</label><br>
@@ -171,16 +172,16 @@ $conn->close();
                     <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-26">冰品</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-27" name="likefoodtag[]"
                     value="27" <?=in_array(27, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-27">麵食</label><br>
+                    <label class="btn btn-outline-secondary ms-2 rounded rounded-4 fw-bold" for="btn-check-27">麵食</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-28" name="likefoodtag[]"
                     value="28" <?=in_array(28, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-28">壽司</label><br>
+                    <label class="btn btn-outline-success ms-2 rounded rounded-4 fw-bold" for="btn-check-28">壽司</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-29" name="likefoodtag[]"
                     value="29" <?=in_array(29, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-29">義大利麵</label><br>
+                    <label class="btn btn-outline-danger ms-2 rounded rounded-4 fw-bold" for="btn-check-29">義大利麵</label><br>
                     <input type="checkbox" class="btn-check" id="btn-check-30" name="likefoodtag[]"
                     value="30" <?=in_array(30, array_column($usertags, 'food_tag_id')) ? 'checked' : ''; ?>>
-                    <label class="btn btn-outline-info ms-2 rounded rounded-4 fw-bold" for="btn-check-30">海鮮</label><br>
+                    <label class="btn btn-outline-primary ms-2 rounded rounded-4 fw-bold" for="btn-check-30">海鮮</label><br>
                 </div>
             </div>
             <div class="input-group mt-2 input-group-lg">
