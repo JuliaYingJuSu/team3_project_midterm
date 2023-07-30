ef55077dd22e8fc48733419872c94ecf5cd68e6c
Will <will8086@gmail.com>
2023-07-30 11:07:39 +0800

    轉移

--- navbar.php
+++ navbar.php
@@ -18,7 +18,7 @@ if(isset($_GET["webpage"])){
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,	initial-scale=1">
     <title>選單</title>
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
@@ -117,7 +117,7 @@ if(isset($_GET["webpage"])){
     
         </main>
     </div>
-    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="./js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
