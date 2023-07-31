f6f1abc5c3483b8ae7c3036efac73512756ad907
Will <will8086@gmail.com>
2023-07-31 16:38:12 +0800

    登陸頁面樣式修改

--- User/login.php
+++ User/login.php
@@ -10,19 +10,23 @@
         .block{
                 width: 300px;
                 height: 250px;
-        }        
+                background-color:#777e5c;
+        }
+        .bg1{
+        background-color:#f1ece2;
+        }       
     </style>
 </head>
 
-<body>
-    <div class="block bg-primary-subtle p-3 position-absolute start-0 end-0 m-auto rounded-2 mt-5">
-        <h2>時時嗑嗑登入系統</h2>
+<body class="bg1">
+    <div class="block p-3 position-absolute start-0 end-0 m-auto rounded-2 mt-5">
+        <h2 class="text-light">食食嗑嗑登入系統</h2>
             <form action="./doLogin.php" method="post">
-                <input type="text" name="email" class="form-control mb-1" placeholder="使用者帳號">
-                <input type="password" name="password1" class="form-control mb-1" placeholder="使用者密碼">
-                <input type="password" name="password2" class="form-control mb-1" placeholder="再輸入一次使用者密碼">
+                <input type="text" name="email" class="form-control mb-2" placeholder="使用者電子郵件">
+                <input type="password" name="password1" class="form-control mb-2" placeholder="使用者密碼">
+                <input type="password" name="password2" class="form-control mb-2" placeholder="再輸入一次使用者密碼">
                 <div class="text-end">
-                <button class="btn btn-info btn-send me-1">送出</button>
+                <button class="btn bg1 btn-send mt-1 fw-bold">送出</button>
                 </div>
             </form>
     </div>
