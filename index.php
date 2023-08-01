1d3747b4fdf24fce3e5a4f17bbad5bc5c682a0ff
will8086 <will8086@gmail.com>
2023-08-02 04:44:07 +0800

    增加首頁圖片

--- index.php
+++ index.php
@@ -9,7 +9,12 @@
 </head>
 
 <body>
-    <div class="position-absolute top-50 start-50 fs-1 bold mt-5 pt-5 " style= "color:#777e5c">歡迎登入食食嗑嗑後台管理系統</div>
+    <div class="container text-center mt-5">
+        <h1 class="fw-bold" style="color:#777e5c">歡迎登入食食嗑嗑後台管理系統</h1>
+        <img src="../img/logo2.png" alt="" width="400" height="300">
+        <hr>
+        <div class="fw-bold ms-5 fw-bold text-start" style="color:#777e5c">版權所有© 2023, 食食嗑嗑</div>
+    </div>
     <script src="./js/bootstrap.bundle.min.js"></script>
 </body>
 
